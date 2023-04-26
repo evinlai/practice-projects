fd9376276a628aea0de39af1135ad5c70e21b67e
ian m <iansm0473@gmail.com>
2023-04-26 13:36:18 +0800

    small clean up on the codes

--- triager tool/javascript.js
+++ triager tool/javascript.js
@@ -1,61 +1,28 @@
-// let newClip = document.querySelector('.err');
+
 // let closeBtn = document.querySelector('.posCloseBtn');
 // let reOpen =  document.querySelector('.reopen');
 let description = document.querySelector('#ctc');
-let transfer = document.querySelector('.transfr')
+let copyBtn = document.querySelector('.copyBtns')
 let mainBtn = document.querySelector('.mainBtns')
 let copyToclip = document.querySelector('#copyText');
 
-// function toClip() {
-
-// // navigator.clipboard.write("value")    // toClips = document.querySelector('btn').value = value;
-
-// console.log(toClips.value);
-// }
-// console.log(toClips);
-
-// const Clips = toClips.getAttribute('value')
-// console.log(reOpen);
-// console.log(newClip.value);
-
-// function updateClipboard(newClips) {
-//     newClips = newClip.value;
-//     navigator.clipboard.writeText(newClips).then(() => {
-//       /* clipboard successfully set */
-      
-//       console.log('copied');
-//     }, () => {
-//       console.log('failed to copy');
-//     });
-//   }
-// closeBtn.addEventListener('click', e => {
-//     const tc = e.target;
-//     navigator.clipboard.writeText(tc.value).then(() => {
-//         console.log('copied')
-//     }, () => {
-//         console.log('failed to copy');
-//     }); 
+// button action - input box text copied to clipboard
+copyBtn.addEventListener('click', e => {
+    console.log("clicked")
+    console.log(e)
     
-// })
-
-// reOpen.addEventListener('click', e => {
-//     const tc = e.target;
-//     navigator.clipboard.writeText(tc.value).then(() => {
-//         console.log('copied')
-//     }, () => {
-//         console.log('failed to copy');
-//     });
+    let cbtn = e.target;
     
-// })
+    if (cbtn.value == undefined){
+        console.log('nothing copied')
+    } else {
+        console.log(cbtn.value)
+        
+    }
+    
+})
 
-// code needed for button clicked copy to clipboard
-
-// copyToclip.addEventListener('click', e => {
-//     console.log(e.type)
-//     // let test = e.value;
-//     // console.log(test.value)
-// })
-
+// button action - copy button value to clipboard
 mainBtn.addEventListener('click', e => {
     let test = e.target;
     
@@ -70,33 +37,3 @@ mainBtn.addEventListener('click', e => {
     }
 
 })
-
-
-description.addEventListener('click', e => {
-    let test = e.target;
-
-
-    navigator.clipboard.writeText(test.value).then(() => {
-        console.log('copied', test.value)
-            }, () => {
-        console.log('failed to copy')
-    });
-
-})
-
-
-transfer.addEventListener('click', e => {
-    let test = e.target;
-    
-    if (test.value == undefined) {
-        console.log('nothing to copy')
-    } else {
-        navigator.clipboard.writeText(test.value).then(() => {
-        console.log('copied', test.value)
-                }, () => {
-        console.log('failed to copy')
-        });
-    }
-
-})
-
