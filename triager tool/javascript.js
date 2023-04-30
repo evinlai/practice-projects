31964dfd1d440649b73a1849b4e817e89efefe0a
ian m <iansm0473@gmail.com>
2023-04-30 10:40:55 +0800

    Now ith working  top buttons with poor coding, still needs modification and cleaning

--- triager tool/javascript.js
+++ triager tool/javascript.js
@@ -1,26 +1,66 @@
 
 // let closeBtn = document.querySelector('.posCloseBtn');
 // let reOpen =  document.querySelector('.reopen');
-let description = document.querySelector('#ctc');
-let copyBtn = document.querySelector('.copyBtns')
-let mainBtn = document.querySelector('.mainBtns')
-let copyToclip = document.querySelector('#copyText');
+let copyBtn = document.querySelector('.copyBtns');
+let mainBtn = document.querySelector('.mainBtns');
+// let copyToclip = document.querySelector('#copyText');
+let callerTxt = document.querySelector('.caller');
+let callName = document.getElementsByClassName('.callerName');
 
-// button action - input box text copied to clipboard
-copyBtn.addEventListener('click', e => {
-    console.log("clicked")
-    console.log(e)
+let descrptn = document.querySelector('.desc');
+let descText = document.querySelector('.descInput');
+
+// //button action - input box text copied to clipboard
+// copyBtn.addEventListener('click', e => {
+//     // console.log("clicked")
+//     // console.log(e)
+//     let cbtn = e.target;
     
-    let cbtn = e.target;
     
-    if (cbtn.value == undefined){
-        console.log('nothing copied')
-    } else {
-        console.log(cbtn.value)
-        
-    }
     
-})
+//         cbtn.value = callerTxt.value;
+//         // console.log(cbtn.value)
+
+//         navigator.clipboard.writeText(cbtn.value).then(() => {
+//             console.log('copied', cbtn.value)
+//                     }, () => {
+//             console.log('nothing to copy')
+//             });
+    
+    
+// })
+// Copy Caller name button
+function manClick() {
+
+ callName.value = callerTxt.value
+
+ if (callName.value == "") {
+    console.log('nothing to copy')
+} else {
+ navigator.clipboard.writeText(callName.value).then(() => {
+    console.log('copied', callName.value)
+            }, () => {
+    console.log('failed to copy')
+    });
+        }    
+}
+
+// Copy Description button
+
+function copyDesc() {
+    
+    descrptn.value = descText.value
+   
+    if (descrptn.value == "") {
+       console.log('nothing to copy')
+   } else {
+    navigator.clipboard.writeText(descrptn.value).then(() => {
+       console.log('copied', descrptn.value)
+               }, () => {
+       console.log('failed to copy')
+       });
+           }    
+   }
 
 // button action - copy button value to clipboard
 mainBtn.addEventListener('click', e => {
